9eab575e2174c40948f0c1067cfe733a565ce985
Evelyn-M <evelyn.muehlhofer@usys.ethz.ch>
2023-04-03 19:15:31 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""A setuptools based setup module.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='osm-flex',
+    version='0.1.0',    
+    description='Python package for flexible data extraction from OpenStreetMap',
+    url='https://github.com/ElcoK/osm-flex',
+    license='GPL-3.0',
+    packages= find_packages()+['data'],
+    install_requires=['geopandas',
+                      'pandas',
+                      'numpy',                     
+                      ],
+    keywords='OpenStreetMap',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+    include_package_data=True
+    
+)
