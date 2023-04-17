becc1edafd369fd7e6725a704fbf17a0666beb04
Chahan Kropf <chahan.kropf@posteo.com>
2023-04-17 20:33:44 +0200

    Fix error with if else

--- src/osm_flex/clip.py
+++ src/osm_flex/clip.py
@@ -188,7 +188,8 @@ def _osmosis_clip(shape, osmpbf_clip_from, osmpbf_output,
     None or subprocess
     """
     osmpbf_clip_from = Path(osmpbf_clip_from)
-    osmpbf_clip_from = osmpbf_clip_from.with_suffix('.osm.pbf') if not osmpbf_clip_from.suffix
+    if not osmpbf_clip_from.suffix
+        osmpbf_clip_from = osmpbf_clip_from.with_suffix('.osm.pbf')
     if not osmpbf_clip_from.is_file():
         raise ValueError(f"OSM file {osmpbf_clip_from} to clip from not found.")
 
