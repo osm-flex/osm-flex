149ed7896ed8f7f7432a4978f0aeebe5f56448ca
Evelyn-M <evelyn.muehlhofer@usys.ethz.ch>
2023-04-18 18:04:01 +0200

    test additions

--- src/osm_flex/tests/test_clip.py
+++ src/osm_flex/tests/test_clip.py
@@ -4,3 +4,92 @@
 test clip functions
 """
 
+import unittest
+import tempfile
+import os
+import shapely
+from osm_flex.clip import get_admin1_shapes, get_country_shape, _simplify_shapelist, _shapely2poly, _build_osmosis_cmd
+
+
+class TestClipping(unittest.TestCase):
+    def test_get_admin1_shapes(self):
+        # Test for valid country code
+        result = get_admin1_shapes("CAN")
+        self.assertIsInstance(result, dict)
+        self.assertGreater(len(result), 0)
+        self.assertIn("Alberta", result.keys())
+
+        # Test for invalid country code
+        with self.assertRaises(LookupError):
+            get_admin1_shapes("XYZ")
+
+        # Test for invalid input type
+        with self.assertRaises(TypeError):
+            get_admin1_shapes(123)
+
+    def test_get_country_shape(self):
+        # Test for valid country code
+        result = get_country_shape("CAN")
+        self.assertIsInstance(result, shapely.geometry.base.BaseGeometry)
+
+        # Test for invalid country code
+        with self.assertRaises(LookupError):
+            get_country_shape("XYZ")
+
+        # Test for invalid input type
+        with self.assertRaises(TypeError):
+            get_country_shape(123)
+
+    def test__simplify_shapelist(self):
+        # Test for valid input
+        geom_list = [shapely.geometry.Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])]
+        result = _simplify_shapelist(geom_list)
+        self.assertEqual(result[0].wkt, 'POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))')
+
+        # Test for invalid input type
+        with self.assertRaises(TypeError):
+            _simplify_shapelist("invalid_input")
+
+    def test__shapely2poly(self):
+        # Test for valid input
+        geom_list = [shapely.geometry.Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])]
+        with tempfile.TemporaryDirectory() as tmpdir:
+            filename = os.path.join(tmpdir, "test_poly")
+            _shapely2poly(geom_list, filename)
+            with open(filename+".poly", "r") as f:
+                content = f.read()
+                self.assertIn("Polygons", content)
+                self.assertIn("0", content)
+                self.assertIn("0.0     0.0", content)
+                self.assertIn("1.0     0.0", content)
+                self.assertIn("1.0     1.0", content)
+                self.assertIn("0.0     1.0", content)
+                self.assertIn("END", content)
+
+        # Test for invalid input type
+        with self.assertRaises(ValueError):
+            _shapely2poly("invalid_input", "test.poly")
+
+    def test__build_osmosis_cmd(self):
+        # Test for valid input
+        shape = [0, 0, 1, 1]
+        osmpbf_clip_from = "/path/to/planet.osm.pbf"
+        osmpbf_output = "/path/to/extract.osm.pbf"
+        result = _build_osmosis_cmd(shape, osmpbf_clip_from, osmpbf_output)
+        expected_result = [
+            'osmosis',
+             '--read-pbf',
+             'file=/path/to/planet.osm.pbf',
+             '--bounding-box',
+             'top=1',
+             'left=0',
+             'bottom=0',
+             'right=1',
+             '--write-pbf',
+             'file=/path/to/extract.osm.pbf']
+        self.assertEqual(result, expected_result)
+        
+
+if __name__ == "__main__":
+    TESTS = unittest.TestLoader().loadTestsFromTestCase(TestClipping)
+    unittest.TextTestRunner(verbosity=2).run(TESTS)
--- src/osm_flex/tests/test_extract.py
+++ src/osm_flex/tests/test_extract.py
@@ -4,3 +4,40 @@
 test extract functions
 """
 
+import unittest
+import shutil
+from pathlib import Path
+import geopandas as gpd
+from osm_flex import extract
+
+# TODO: how to test extraction? put a small osm-pbf file into a test/data folder?
+
+class Testextract(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.osm_file = 'test.osm.pbf'
+        cls.geojson_file = 'test.geojson'
+        cls.osm_path = Path(__file__).parent / 'data' / cls.osm_file
+        cls.geojson_path = Path(__file__).parent / 'data' / cls.geojson_file
+        # create a test output directory
+        cls.test_output_dir = Path(__file__).parent / 'output'
+        cls.test_output_dir.mkdir(exist_ok=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        # remove the test output directory
+        shutil.rmtree(cls.test_output_dir)
+
+    def test_retrieve(self):
+        pass
+
+    def test_retrieve_cis(self):
+        pass
+
+    def test__query_builder(self):
+        pass
+
+if __name__ == "__main__":
+    TESTS = unittest.TestLoader().loadTestsFromTestCase(Testextract)
+    unittest.TextTestRunner(verbosity=2).run(TESTS)                              
