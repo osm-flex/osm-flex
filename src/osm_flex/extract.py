790284f915ab8725c4e8059ba2391ef0123337a3
Chahan Kropf <chahan.kropf@posteo.com>
2023-08-28 16:59:26 +0200

    Add simple method to remove polygons

--- src/osm_flex/extract.py
+++ src/osm_flex/extract.py
@@ -95,8 +95,8 @@ def extract(osm_path, geo_type, osm_keys, osm_query=None):
     E.g. tower:type is called tower_type, since it would interfere with the
     SQL syntax otherwise, but still tower:type in the osmconf.ini
     3) If the osm_query is left empty (None), then all objects will be parsed
-    for which the first entry of osm_keys is not Null. E.g. if osm_keys = 
-    ['building', 'name'] and osm_query = None, then all items matching 
+    for which the first entry of osm_keys is not Null. E.g. if osm_keys =
+    ['building', 'name'] and osm_query = None, then all items matching
     building=* will be parsed.
 
     See also
@@ -109,7 +109,7 @@ def extract(osm_path, geo_type, osm_keys, osm_query=None):
     """
     if not Path(osm_path).is_file():
         raise ValueError(f"the given path is not a file: {osm_path}")
-    
+
     osm_path = str(osm_path)
     constraint_dict = {
         'osm_keys' : osm_keys,
@@ -175,7 +175,7 @@ def extract_cis(osm_path, ci_type):
 
     # features consisting in multipolygon results:
     elif ci_type in ['air']:
-        gdf = extract(osm_path, 'multipolygons', 
+        gdf = extract(osm_path, 'multipolygons',
                       DICT_CIS_OSM[ci_type]['osm_keys'],
                       DICT_CIS_OSM[ci_type]['osm_query'])
 
@@ -194,3 +194,33 @@ def extract_cis(osm_path, ci_type):
         LOGGER.warning('feature not in DICT_CIS_OSM. Returning empty gdf')
         gdf = gpd.GeoDataFrame()
     return gdf
+
+def remove_small_polygons(gdf, min_area):
+    """Remove (multi-)polygons of area smaller than min_area
+    Points and lines are untouched.
+
+    Note: a buffer of 1e-10 is added to invalid geometries
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame
+        geodataframe with polygons
+    min_area : float
+        minimal value of area
+
+    Return
+    ------
+    GeoDataFrame:
+        entry geodataframe without (multi-)polygons smaller than min_area
+    """
+
+    gdf_temp = gdf.copy()
+
+    def make_valid(geometry):
+        if geometry.is_valid:
+            return geometry
+        return geometry.buffer(1e-10)
+
+    gdf_temp['geometry'] = gdf_temp.apply(lambda row: make_valid(row.geometry), axis=1)
+
+    return gdf_temp[(gdf_temp['geometry'].area > min_area) or (gdf_temp['geometry'].area == 0)]
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -14,10 +14,11 @@ test extraction functions
 import unittest
 import geopandas as gpd
 import numpy as np
-from osm_flex.extract import extract, extract_cis, _query_builder
+import shapely as sh
+from osm_flex.extract import extract, extract_cis, _query_builder, remove_small_polygons
 from pathlib import Path
 
-PATH_TEST_DATA = Path(__file__).parent / 'data' 
+PATH_TEST_DATA = Path(__file__).parent / 'data'
 OSM_FILE = PATH_TEST_DATA / 'test.osm.pbf'
 
 class TestExtractionFunctions(unittest.TestCase):
@@ -26,53 +27,53 @@ class TestExtractionFunctions(unittest.TestCase):
         """
         test function extract()
         """
-        gdf_mp = extract(OSM_FILE, 'multipolygons',  ['name', 'building'], 
+        gdf_mp = extract(OSM_FILE, 'multipolygons',  ['name', 'building'],
                          "building='yes'")
         self.assertIsInstance(gdf_mp, gpd.GeoDataFrame)
-        self.assertEqual(set(gdf_mp.columns), 
+        self.assertEqual(set(gdf_mp.columns),
                          set(['osm_id', 'building', 'name', 'geometry']))
-        self.assertEqual(np.unique(gdf_mp.geometry.type), 
+        self.assertEqual(np.unique(gdf_mp.geometry.type),
                          np.array(['MultiPolygon']))
         self.assertTrue(len(gdf_mp)==4202)
-        
+
         gdf_mp2 = extract(OSM_FILE, 'multipolygons',  ['building', 'name'])
         self.assertIsInstance(gdf_mp2, gpd.GeoDataFrame)
-        self.assertEqual(set(gdf_mp2.columns), 
+        self.assertEqual(set(gdf_mp2.columns),
                          set(['osm_id', 'building', 'name', 'geometry']))
-        self.assertEqual(np.unique(gdf_mp2.geometry.type), 
+        self.assertEqual(np.unique(gdf_mp2.geometry.type),
                          np.array(['MultiPolygon']))
         self.assertEqual(len(gdf_mp2),5050)
         self.assertFalse(any(elem is None for elem in gdf_mp2.building))
-        
-        
-        gdf_line = extract(OSM_FILE, 'lines', ['name', 'highway'], 
+
+
+        gdf_line = extract(OSM_FILE, 'lines', ['name', 'highway'],
                            "highway='residential'")
         self.assertIsInstance(gdf_line, gpd.GeoDataFrame)
-        self.assertEqual(set(gdf_line.columns), 
+        self.assertEqual(set(gdf_line.columns),
                          set(['osm_id', 'name', 'highway', 'geometry']))
-        self.assertEqual(np.unique(gdf_line.geometry.type), 
+        self.assertEqual(np.unique(gdf_line.geometry.type),
                          np.array(['LineString']))
         self.assertTrue(len(gdf_line)==1807)
-        
+
         gdf_line2 = extract(OSM_FILE, 'lines', ['highway','name'], None)
         self.assertIsInstance(gdf_line2, gpd.GeoDataFrame)
-        self.assertEqual(set(gdf_line2.columns), 
+        self.assertEqual(set(gdf_line2.columns),
                          set(['osm_id', 'name', 'highway', 'geometry']))
-        self.assertEqual(np.unique(gdf_line2.geometry.type), 
+        self.assertEqual(np.unique(gdf_line2.geometry.type),
                          np.array(['LineString']))
         self.assertEqual(len(gdf_line2),3266)
         self.assertFalse(any(elem is None for elem in gdf_line2.highway))
-        
+
         gdf_line3 = extract(OSM_FILE, 'lines', ['highway'], None)
         self.assertIsInstance(gdf_line3, gpd.GeoDataFrame)
-        self.assertEqual(set(gdf_line3.columns), 
+        self.assertEqual(set(gdf_line3.columns),
                          set(['osm_id', 'highway', 'geometry']))
-        self.assertEqual(np.unique(gdf_line3.geometry.type), 
+        self.assertEqual(np.unique(gdf_line3.geometry.type),
                          np.array(['LineString']))
         self.assertEqual(len(gdf_line3),3266)
         self.assertFalse(any(elem is None for elem in gdf_line3.highway))
 
-        
+
         # TODO: test with invalid geo_type
 
     def test_extract_cis(self):
@@ -84,15 +85,15 @@ class TestExtractionFunctions(unittest.TestCase):
         self.assertTrue(np.any([entry =='school' for entry in gdf_schools.amenity]))
         self.assertTrue(len(gdf_schools)==215)
         self.assertTrue('amenity' in gdf_schools.columns)
-        
+
         gdf_roads = extract_cis(OSM_FILE, 'road')
         self.assertTrue(np.any([entry =='tertiary' for entry in gdf_roads.highway]))
         self.assertTrue(np.any([entry =='residential' for entry in gdf_roads.highway]))
         self.assertTrue(len(gdf_roads)==2603)
         self.assertTrue('name' in gdf_roads.columns)
-        
+
         # TODO: test with invalid ci-argument
-        
+
     def test__query_builder(self):
         """
         test function _query_builder()
@@ -103,26 +104,45 @@ class TestExtractionFunctions(unittest.TestCase):
         query = _query_builder(geo_type, constraint_dict)
         self.assertEqual(query,
                          "SELECT osm_id,name,highway FROM points WHERE highway='residential'")
-        
+
         geo_type = 'points'
         constraint_dict = {'osm_keys': ['name', 'highway'],
                            'osm_query' : None}
         query = _query_builder(geo_type, constraint_dict)
         self.assertEqual(query,
                          'SELECT osm_id,name,highway FROM points WHERE name IS NOT NULL')
-        
-      
+
+
     def test_qb_error(self):
         geo_type = 'points'
         constraint_dict2 = {'wrong_key': ['name', 'highway'],
                            'osm_query' :  "highway='residential'"}
-        
+
         with self.assertRaises(KeyError) as context:
             _query_builder(geo_type, constraint_dict2)
         # TODO: how to check now that raises either keyerror or key error with message
         #self.assertTrue('osm_keys' in context.exception)
         #self.assertTrue(KeyError in context.exception)
-       
+
+    def test_remove_small_polygons(self):
+        coords1 = ((0., 0.), (0., 1.), (1., 1.), (1., 0.), (0., 0.)) #area=1
+        polygon1 = sh.Polygon(coords1)
+        coords2 = ((0., 0.), (0., 0.1), (0.1, 0.1), (0.1, 0.), (0., 0.)) #area=0.01
+        polygon2 = sh.Polygon(coords2)
+        gdf = gpd.GeoDataFrame(
+            geometry = [
+                sh.Point(1.0, -1.0),
+                LineString([[0, 0], [1, 0], [1, 1]]),
+                polygon1,
+                polygon2
+            ])
+        gdf_no_small_poly = gpd.GeoDataFrame(
+            geometry = [
+                sh.Point(1.0, -1.0),
+                LineString([[0, 0], [1, 0], [1, 1]]),
+                polygon1,
+            ])
+        gpd.testing.assert_geodataframe_equal(remove_small_polygons(gdf, 0.5), gdf)
 if __name__ == "__main__":
     TESTS = unittest.TestLoader().loadTestsFromTestCase(TestExtractionFunctions)
     unittest.TextTestRunner(verbosity=2).run(TESTS)
