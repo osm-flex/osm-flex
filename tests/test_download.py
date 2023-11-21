5e22a316ff5fa2974b6f00b72a69f5d832324b19
Lukas Riedel <mail@lukasriedel.com>
2023-11-21 14:22:44 +0100

    Update download.py module
    * Add function `_download_file` for downloading a single file.
    * Make download functions return the resulting filepath.
    * Improve error on unsupported data types.
    * Update download tests.

--- tests/test_download.py
+++ tests/test_download.py
@@ -16,70 +16,66 @@ import tempfile
 import os
 import shapely
 from pathlib import Path
-from osm_flex.download import (_create_gf_download_url, 
-                               get_country_geofabrik, 
-                               get_region_geofabrik, 
-                               get_planet_file)
+from osm_flex.download import (
+    _create_gf_download_url,
+    get_country_geofabrik,
+    get_region_geofabrik,
+    get_planet_file,
+)
+
 
 class TestDownload(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_create_gf_download_url(self):
-        
-        # TODO: assert raises KeyError
-        #iso3 = 'ATG'
-        #file_format = 'pbf'
-        #_create_gf_download_url(iso3, file_format)
-        
-        # TODO: assert raises KeyError
-        #iso3 = 'DEU'
-        #file_format = 'pdf'
-        #_create_gf_download_url(iso3, file_format)
-        
-        
-        iso3 = 'DEU'
-        file_format = 'pbf'
-        url = _create_gf_download_url(iso3, file_format)
-        self.assertEqual(url, 'https://download.geofabrik.de/europe/germany-latest.osm.pbf')
-        
-        iso3 = 'DEU'
-        file_format = 'shp'
-        url = _create_gf_download_url(iso3, file_format)
-        self.assertEqual(url, 'https://download.geofabrik.de/europe/germany-latest-free.shp.zip')
-        
-        iso3 = 'RUS-A'
-        file_format = 'pbf'
-        url = _create_gf_download_url(iso3, file_format)
-        self.assertEqual(url, 'https://download.geofabrik.de/asia/russia-latest.osm.pbf')
-        
-        iso3 = 'RUS-E'
-        file_format = 'pbf'
-        url = _create_gf_download_url(iso3, file_format)
-        self.assertEqual(url, 'https://download.geofabrik.de/europe/russia-latest.osm.pbf')
-    
-        
+        with self.assertRaisesRegex(
+            KeyError, "The provided iso3 seems not to be available on Geofabrik.de"
+        ):
+            _create_gf_download_url("ATG", "pbf")
+
+        with self.assertRaisesRegex(NotImplementedError, "Invalid file format 'pdf'"):
+            _create_gf_download_url("DEU", "pdf")
+
+        url = _create_gf_download_url("DEU", "pbf")
+        self.assertEqual(
+            url, "https://download.geofabrik.de/europe/germany-latest.osm.pbf"
+        )
+
+        url = _create_gf_download_url("DEU", "shp")
+        self.assertEqual(
+            url, "https://download.geofabrik.de/europe/germany-latest-free.shp.zip"
+        )
+
+        url = _create_gf_download_url("RUS-A", "pbf")
+        self.assertEqual(
+            url, "https://download.geofabrik.de/asia/russia-latest.osm.pbf"
+        )
+
+        url = _create_gf_download_url("RUS-E", "pbf")
+        self.assertEqual(
+            url, "https://download.geofabrik.de/europe/russia-latest.osm.pbf"
+        )
+
     def test_get_country_geofabrik(self):
-        
-        iso3 = 'IC'
+        iso3 = "IC"
         with tempfile.TemporaryDirectory() as tmpdir:
             filename = os.path.join(tmpdir, "canary-islands-latest.osm.pbf")
-            get_country_geofabrik(iso3, save_path=tmpdir)
+            return_value = get_country_geofabrik(iso3, save_path=tmpdir)
+            self.assertEqual(return_value, Path(filename))
             self.assertTrue(os.path.exists(filename))
 
     def test_get_region_geofabrik(self):
-
-        region = 'central-america'
+        region = "central-america"
         with tempfile.TemporaryDirectory() as tmpdir:
             filename = os.path.join(tmpdir, "central-america-latest.osm.pbf")
-            get_region_geofabrik(region, save_path=tmpdir)
+            return_value = get_region_geofabrik(region, save_path=tmpdir)
+            self.assertEqual(return_value, Path(filename))
             self.assertTrue(os.path.exists(filename))
 
+    @unittest.skip("File too large to test download")
     def test_get_planet_file(self):
-        pass
+        self.fail("No test implemented")
+
 
 if __name__ == "__main__":
     TESTS = unittest.TestLoader().loadTestsFromTestCase(TestDownload)
     unittest.TextTestRunner(verbosity=2).run(TESTS)
-
